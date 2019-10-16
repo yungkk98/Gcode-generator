9ca19fac5c160e2f11c9d851b7451e4fcac0a3ad
HyeongJin Kim <yungkk87@gmail.com>
2019-10-16 20:50:00 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,39 +1,19 @@
-from math import *
-from tkinter import *
-import pyclipper
+from stl_normalize import GcodeGenerater
 
-r = 200
+gcode_gen = GcodeGenerater()
+gcode_gen.unit_E = 1.0
+gcode_gen.infile = './hexhab3x_3rd-1_R.stl'
 
+gcode_gen.analyze()
 
-master = Tk()
-w = Canvas(master, width=r*6, height=r*4)
-w.pack()
+print('layer count: ', len(gcode_gen.z_range))
+print(gcode_gen.z_range)
 
-subjs = []
-pco = pyclipper.PyclipperOffset(arc_tolerance=100)
-for xoff in [0, int(r/0.45)]:
-    subj = [
-        (
-            (r*(1+(sin(6*a*pi/180)-1)/3)*cos(a*pi/180)+r*2+xoff),
-            (r*(1+(sin(6*a*pi/180)-1)/3)*sin(a*pi/180)+r*2)
-        )
-        for a in range(0, 360, 1)
-    ]
-    subj.append(subj[0])
-    subjs.append(subj)
-    subj = pyclipper.scale_to_clipper(subj, 1000)
-    pco.AddPath(subj, pyclipper.JT_ROUND, pyclipper.ET_CLOSEDPOLYGON)
-    #pco.AddPath(subj, pyclipper.JT_SQUARE, pyclipper.ET_CLOSEDPOLYGON)
-    #pco.AddPath(subj, pyclipper.JT_MITER, pyclipper.ET_CLOSEDPOLYGON)
+print(len(gcode_gen.paths[44]))
+print(gcode_gen.paths[44])
+print(len(gcode_gen.paths[45]))
+print(gcode_gen.paths[45])
 
-solution = pco.Execute(-r/10*1000)
-solution = pyclipper.scale_from_clipper(solution, 1000)
-
-for path in subjs:
-    w.create_line(path, fill="red", width=1)
-
-for path in solution:
-    path.append(path[0])
-    w.create_line(path, fill="blue", width=1)
-
-mainloop()
+for i in range(len(gcode_gen.z_range)):
+    if len(gcode_gen.paths[i][1]) == 1:
+        print(i)
